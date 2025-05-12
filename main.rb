f457950bc14ab0f7a83609fca82675915eb8ecbd
samuel <samiehave9@gmail.com>
2025-05-12 12:08:27 +0300

    Class to instantiate Player and SecretWord objects, and methods provide the game logic

--- main.rb
+++ main.rb
@@ -3,3 +3,17 @@ require './player'
 require './secret_word'
 require './serialize'
 
+# Class to instantiate Player and SecretWord objects, and methods provide the game logic
+class Game
+  # Uses the Serialize module as well
+  include Serialize
+  attr_accessor :word, :player, :display, :life
+
+  def initialize
+    @word = SecretWord.new # SecretWord object
+    @player = Player.new # Player object
+    @display = '' # Displayed letters
+    @life = 8 # Number of lives
+    load_or_new # Load the game state or start a new game
+  end
+end
