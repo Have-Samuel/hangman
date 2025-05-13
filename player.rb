3562e35328cda1e8ebb21fcc726ebd62e415c793
samuel <samiehave9@gmail.com>
2025-05-13 12:35:40 +0300

    Get player input and pass to validation method, unless save

--- player.rb
+++ player.rb
@@ -3,9 +3,9 @@ class Player
   attr_accessor :guess, :misses, :guess_history
 
   def initialize
-    @guess = ''
-    @misses = []
-    @guess_history = []
+    @guess = '' # Initialize guess to an empty string, that involves the guesses that the player has made
+    @misses = [] # Initialize misses to an empty array, that involves the letters that the player has guessed and are not in the word
+    @guess_history = [] # Initialize guess_history to an empty array, that involves the letters that the player has guessed, whether right or wrong
   end
 
   # Get player input and pass to validation method, unless save
@@ -13,9 +13,11 @@ class Player
     puts 'Enter your guess (one letter a - z), or save.'
     input = gets.chomp.downcase
     if input == 'save'
-      @guess = input
+      @guess = input # if the player guess is save, set the guess to save
     else
-      validate_input(input)
+      validate_input(input) # Else validate the input
     end
   end
+
+  
 end
